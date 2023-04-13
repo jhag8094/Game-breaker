cd44d7f6e25def3f73ec24a4dc7e076d3a4616a6
jhag8094 <123817237+jhag8094@users.noreply.github.com>
2023-04-13 16:42:03 +0530

    Add files via upload

---
+++ source.java
@@ -0,0 +1,25 @@
+/*hh
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author asus
+ */
+import javax.swing.JFrame;
+public class source {
+    
+	 public static void main(String[] args) {
+	        JFrame obj = new JFrame();
+	        GamePlay gameplay = new GamePlay();
+	        obj.setBounds(10,10,700,600);
+	        obj.setTitle("BrickBreaker");
+	        obj.setResizable(false);
+	        obj.setVisible(true);
+	        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        obj.add(gameplay);
+         }
+    
+}
